b774a7c3f500fb66366951201f679cc4e4fe4e47
Jean-François Defez <jean-francois@ayctor.com>
2021-08-22 19:50:20 +0200

    fix(tests) fixing filename

--- tests/Unit/GraphqlRequestTest.php
+++ tests/Unit/GraphqlRequestTest.php
@@ -3,8 +3,6 @@
 namespace Jdefez\LaravelGraphql\Tests;
 
 use Jdefez\LaravelGraphql\Facades\Graphql;
-use Jdefez\LaravelGraphql\Field;
-use Jdefez\LaravelGraphql\QueryBuilder;
 use Jdefez\LaravelGraphql\Requestable;
 use Jdefez\LaravelGraphql\tests\TestCase;
 
@@ -18,24 +16,6 @@ class GraphqlRequestTest extends TestCase
         return $this->assertInstanceOf(
             Requestable::class,
             Graphql::request($this->api_url)
-        );
-    }
-
-    /** @test */
-    public function it_can_send_a_request()
-    {
-        $request = Graphql::request($this->api_url);
-        $query = QueryBuilder::query()
-            ->user(['id' => 1], fn(Field $user) => $user->email()
-                ->name()
-                ->id()
-            );
-        $response = $request->get($query->toString());
-        dd($response);
-
-        $this->assertEquals(
-            'QUERY { user(id: 1) { email name id }}',
-            $query->toString()
         );
     }
 }
