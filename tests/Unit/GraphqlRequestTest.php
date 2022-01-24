2ed5004174e1ab9965da2cdf9bb5938788b754a3
jdefez <jdefez@gmail.com>
2022-01-24 20:35:49 +0100

    test(GraphqlRequestTest): removing useless tests

--- tests/Unit/GraphqlRequestTest.php
+++ tests/Unit/GraphqlRequestTest.php
@@ -2,8 +2,6 @@
 
 namespace Jdefez\LaravelGraphql\tests\Unit;
 
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Support\Facades\Http;
 use Jdefez\LaravelGraphql\Facades\Graphql;
 use Jdefez\LaravelGraphql\Request\Client;
 use Jdefez\LaravelGraphql\Request\Requestable;
@@ -23,73 +21,17 @@ class GraphqlRequestTest extends TestCase
     }
 
     /** @test */
-    public function it_can_instanciate_a_request()
+    public function it_uses_inputable()
     {
-        return $this->assertInstanceOf(
+        $this->markTestIncomplete('todo: implement');
+    }
+
+    /** @test */
+    public function it_can_instanciate_a_request(): void
+    {
+        $this->assertInstanceOf(
             Requestable::class,
             Graphql::request($this->api_url)
         );
-    }
-
-    /** @test */
-    public function it_handles_graphql_validation_errors()
-    {
-        $this->expectException(RequestException::class);
-        $this->httpFake([
-            'errors' => [
-                [
-                    'message' => 'Cannot query field "unknownQuery" on type "Query".',
-                    'extensions' => [
-                        'validation' => [
-                            'key' => ['some reason'],
-                        ],
-                        'category' => 'validation'
-                    ],
-                    'locations' => [
-                        [
-                            'line' => 1,
-                            'column' => 9
-                        ]
-                    ]
-                ]
-            ]
-        ], 500);
-
-        //$this->expectExceptionMessage('Cannot query field "unknownQuery" on type "Query". (validation): some reason');
-        $this->client->get('some query');
-    }
-
-    /** @test */
-    public function response_is_the_query_object_him_self()
-    {
-        $this->httpFake([
-            'data' => [
-                'user' => [
-                    'email' => 'test@gmail.com',
-                    'name' => 'test',
-                    'id' => 1
-                ]
-            ]
-        ]);
-        $response = $this->client->get('some query');
-
-        // todo: fetch interfacage response handler and fix this tests
-
-        $this->assertInstanceOf('stdClass', $response->object());
-        $this->assertObjectHasAttribute('email', $response->user);
-        $this->assertObjectHasAttribute('name', $response->user);
-        $this->assertObjectHasAttribute('id', $response->user);
-    }
-
-    public function it_handles_validation_exception()
-    {
-        $this->markTestIncomplete('todo implement');
-    }
-
-    private function httpFake($responseBody, int $responseCode = 200)
-    {
-        Http::fake([
-            'localhost*' => Http::response($responseBody, $responseCode)
-        ]);
     }
 }
